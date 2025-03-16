0399324f2232e8f94c14031224eb9fa5ff702699
Thauan <dev.thauan.rodrigues@gmail.com>
2025-03-16 18:58:59 -0300

    feat: added FindAll with pages function

--- internal/infra/database/product_db.go
+++ internal/infra/database/product_db.go
@@ -19,6 +19,20 @@ func (p *Product) Create(product *entity.Product) error {
 	return p.DB.Create(product).Error
 }
 
+func (p *Product) FindAll(page, limit int, sort string) ([]entity.Product, error) {
+	var products []entity.Product
+	var err error
+	if sort != "asc" && sort != "desc" && sort != "" {
+		sort = "asc"
+	}
+	if page != 0 && limit != 0 {
+		err = p.DB.Offset((page - 1) * limit).Order("created_at" + sort).Find(&products).Error
+	} else {
+		err = p.DB.Order("created_at" + sort).Find(&products).Error
+	}
+	return products, err
+}
+
 func (p *Product) FindById(id string) (*entity.Product, error) {
 	var product entity.Product
 	err := p.DB.Find(&product, "id = ?", id).Error
@@ -34,4 +48,12 @@ func (p *Product) Update(product *entity.Product) error {
 		return err
 	}
 	return p.DB.Save(product).Error
+}
+
+func (p *Product) Delete(id string) error {
+	db, err := p.FindById(id)
+	if err != nil {
+		return err
+	}
+	return p.DB.Delete(db).Error
 }
